3a905c68c68769ef08023d609428eeea4f7ffd8c
Sergey Bochkov <bochkovsa@gmail.com>
2024-04-11 12:31:49 +0500

    improved encrypt/decrypt

--- src/main/java/sb/bdev/crypto/Aes.java
+++ src/main/java/sb/bdev/crypto/Aes.java
@@ -1,44 +1,84 @@
 package sb.bdev.crypto;
 
-import java.security.GeneralSecurityException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
-import java.util.Base64;
+import lombok.RequiredArgsConstructor;
+
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-
-import lombok.RequiredArgsConstructor;
+import java.nio.ByteBuffer;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
 
 @RequiredArgsConstructor
 public final class Aes {
 
-    private static final IvParameterSpec SPEC = new IvParameterSpec(new byte[16]);
-    private static final String ALGORITHM = "AES/CBC/PKCS5Padding";
+    private static final String ALGORITHM = "AES/GCM/NoPadding";
+    private static final String FACTORY = "PBKDF2WithHmacSHA256";
+    private static final int IV_LENGTH = 12;
+    private static final int SALT_LENGTH = 16;
+    private static final int ITER_COUNT = 65536;
+    private static final int KEY_LENGTH = 256;
 
     private final String password;
-    private final String salt;
-
-    public SecretKey key(String password, String salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
-        return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
-    }
 
     public String encrypt(String source) throws GeneralSecurityException {
+        byte[] iv = randomBytes(IV_LENGTH);
+        AlgorithmParameterSpec spec = new GCMParameterSpec(128, iv);
+
+        byte[] salt = randomBytes(SALT_LENGTH);
+        SecretKey secretKey = key(password, salt);
+
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, key(password, salt), SPEC);
-        return Base64.getEncoder().encodeToString(cipher.doFinal(source.getBytes()));
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, spec);
+
+        byte[] encodedBytes = cipher.doFinal(source.getBytes());
+        byte[] out = ByteBuffer.allocate(IV_LENGTH + SALT_LENGTH + encodedBytes.length)
+                .put(iv)
+                .put(salt)
+                .put(encodedBytes)
+                .array();
+        return Base64.getEncoder().encodeToString(out);
     }
 
     public String decrypt(String source) throws GeneralSecurityException {
+        byte[] in = Base64.getDecoder().decode(source);
+        ByteBuffer buffer = ByteBuffer.wrap(in);
+
+        byte[] iv = new byte[IV_LENGTH];
+        buffer.get(iv);
+
+        byte[] salt = new byte[SALT_LENGTH];
+        buffer.get(salt);
+
+        byte[] encodedBytes = new byte[buffer.remaining()];
+        buffer.get(encodedBytes);
+
+        SecretKey secretKey = key(password, salt);
+        AlgorithmParameterSpec spec = new GCMParameterSpec(128, iv);
+
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, key(password, salt), SPEC);
-        return new String(cipher.doFinal(Base64.getDecoder().decode(source)));
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, spec);
+        return new String(cipher.doFinal(encodedBytes));
+    }
+
+    public SecretKey key(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, ITER_COUNT, KEY_LENGTH);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance(FACTORY);
+        return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+    }
+
+    private byte[] randomBytes(int length) {
+        byte[] bytes = new byte[length];
+        new SecureRandom().nextBytes(bytes);
+        return bytes;
     }
 
 }
--- src/test/java/sb/bdev/crypto/AesTest.java
+++ src/test/java/sb/bdev/crypto/AesTest.java
@@ -1,36 +1,26 @@
 package sb.bdev.crypto;
 
-import java.security.GeneralSecurityException;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class AesTest {
+import java.security.GeneralSecurityException;
 
-    String pass = "f0v20fv sdvkejrt2fsd";
-    String salt = "cvmm2vscgdfhy";
+class AesTest {
 
     @Test
     void test() throws GeneralSecurityException {
         String plainText = "hello_world";
         String password = "njg2n03rqsdsdg";
-        String salt = "vcnvwetwehfgjghj";
-        Aes aes = new Aes(password, salt);
+        Aes aes = new Aes(password);
         String cipherText = aes.encrypt(plainText);
         String decryptText = aes.decrypt(cipherText);
         Assertions.assertThat(decryptText).isEqualTo(plainText);
     }
 
     @Test
-    void testEncrypt() throws GeneralSecurityException {
-        String text = "hello world";
-        String enc = new Aes(pass, salt).encrypt(text);
-        Assertions.assertThat(enc).isEqualTo("3HCXlRRDpq/m2z0V8yba3A==");
-    }
-
-    @Test
     void testDecrypt() throws GeneralSecurityException {
-        String dec = new Aes(pass, salt).decrypt("3HCXlRRDpq/m2z0V8yba3A==");
+        String dec = new Aes("f0v20fv sdvkejrt2fsd")
+                .decrypt("QTQGiEERKX6gcLGzRai792SBnYuIInxk4xJmZwv7HBBv9WOsGsSZOZJJsqL6zjpjXPasSHDouQ==");
         Assertions.assertThat(dec).isEqualTo("hello world");
     }
 }
